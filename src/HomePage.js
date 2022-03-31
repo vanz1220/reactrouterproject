c07fe953976ea45dd1ff86e15411057c626b57f4
vanz1220 <92617472+vanz1220@users.noreply.github.com>
2022-03-31 10:16:15 +0800

    ayusss

--- src/HomePage.js
+++ src/HomePage.js
@@ -4,7 +4,6 @@ import AnimatedText from 'react-animated-text-content';
 export default function HomePage() {
   return (
     <div class="About-Height">
-    
     <h1 class="wew">
       <AnimatedText
         type="chars" // animate words or chars
@@ -24,6 +23,27 @@ export default function HomePage() {
         rootMargin="20%"
       >
         I ❣ REACT JS 🍻
+      </AnimatedText>
+    </h1>
+    <h1 class="wew">
+      <AnimatedText
+        type="chars" // animate words or chars
+        animation={{
+          x: '200px',
+          y: '-20px',
+          scale: 1.1,
+          ease: 'ease-in-out',
+        }}
+        animationType="diagonal"
+        interval={0.2}
+        duration={2.5}
+        tag="p"
+        className="animated-paragraph"
+        includeWhiteSpaces
+        threshold={0.1}
+        rootMargin="20%"
+      >
+        THIS HOME PAGE
       </AnimatedText>
     </h1>
   </div>
